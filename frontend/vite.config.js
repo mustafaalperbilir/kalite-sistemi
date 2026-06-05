0105e47dcabf22c67d26f7127502f8f2571c1fdf
Mustafa Alper Bilir <alperbilir29@gmail.com>
2026-06-05 17:52:08 +0300

    Revert singlefile and split chunks to bypass Fortinet size limits on single files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
   base: '/',
+  build: {
+    assetsDir: '',
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]-[hash].js`,
+        chunkFileNames: `[name]-[hash].js`,
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
